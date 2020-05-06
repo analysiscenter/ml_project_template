d359de2cb3282ca006942dc0735c59ad8d49cdf8
a-arefina <7520522+a-arefina@users.noreply.github.com>
2020-05-06 13:19:42 +0300

    added batchflow as core lib (#7) + removed option for adding lib as submodule

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,33 +3,28 @@ import subprocess
 import json
 
 
-mode = '{{ cookiecutter.libmode }}'
-print("Mode is:{}.".format(mode))
-
 library = '{{ cookiecutter.include_library }}'
+
+print("Library is", library)
 
 libpaths = {
 'SeismicPro': 'https://github.com/gazprom-neft/SeismicPro.git',
 'SeismiQB': 'https://github.com/gazprom-neft/seismiqb.git',
 'PetroFlow': 'https://github.com/gazprom-neft/petroflow.git',
+'batchflow': 'https://github.com/analysiscenter/batchflow.git'
 # 'testlib': 'https://github.com/analysiscenter/segy.git',
 }
 
-if mode is "git.submodule":
+print("initializing git repo ...")
+subprocess.run(['git', 'init'])
 
-    subprocess.run(['git', 'init'])
+if library is not "no":
+
+    print("adding submodule", library, "...")
 
     path = libpaths[library]
     subprocess.run(['git', 'submodule', 'add', path])
     subprocess.run(['git', 'submodule', 'update', '--init', '--recursive'])
 
-    with open('extra/.cookiecutter.json') as inpf:
-        context = json.load(inpf)
-
-    context['libmode'] = 'no_submodules'
-
-    with open('extra/.cookiecutter.json', 'w') as outf:
-        json.dump(context, outf, indent=4)
-
-    subprocess.run(['git', 'add', '*'])
-    subprocess.run(['git', 'commit', '-m', '"initial commit"'])
+subprocess.run(['git', 'add', '*'])
+subprocess.run(['git', 'commit', '-m', '"initial commit"'])
